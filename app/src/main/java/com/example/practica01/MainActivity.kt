428ff8b5b1f7af93237fed1e6c854a2e457a594b
daviidvh <davvel833@alu.centroafuera.es>
2022-11-22 21:36:29 +0100

    Ejer2

--- app/src/main/java/com/example/practica01/MainActivity.kt
+++ app/src/main/java/com/example/practica01/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.practica01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     private var onCreate = 0
@@ -11,6 +13,10 @@ class MainActivity : AppCompatActivity() {
     private var onRestart = 4
     private var onStop = 5
     private var onDestroy = 6
+    var t: String? =null
+    var txt1: EditText?=null
+    var txt2: EditText?=null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,4 +57,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("onDestroy $onDestroy")
     }
+
+    override fun onSaveInstanceState(guardarEstado: Bundle)  {
+        super.onSaveInstanceState(guardarEstado)
+        txt1 = findViewById (R.id.editText)
+        t = txt1!!.text.toString();
+        guardarEstado.putString("text1", t);
+
+
+        txt2 = findViewById (R.id.editText2)
+        t = txt2!!.text.toString();
+        guardarEstado.putString("text2", t);
+
+
+    }
+
+    override fun onRestoreInstanceState(recuperarEstado: Bundle) {
+        super.onRestoreInstanceState(recuperarEstado)
+        t = recuperarEstado.getString("text1")
+        txt1!!.setText(t)
+
+        t = recuperarEstado.getString("text2")
+        txt2!!.setText(t)
+    }
 }
+
